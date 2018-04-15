2ce803564df31738e251864f031adb7ce1f503c1
Prashanthi Boravelly <prashanthi.boravelly@optus.com.au>
2018-04-15 15:11:27 +1000

    Created utility methods to fetch response from server.

--- CollectionViewDemo/Model/NetworkManager.swift
+++ CollectionViewDemo/Model/NetworkManager.swift
@@ -12,3 +12,63 @@ enum Result<T, E: Error> {
     case success(T)
     case failure(E)
 }
+
+class NetworkManager {
+    
+    class func responseData(urlString: String,
+                            completion: @escaping (Result<Data, CustomError>) -> Void) {
+        if let baseUrl = URL(string: urlString) {
+            let request = URLRequest(url: baseUrl)
+            
+            // Sending request to fetch data.
+            URLSession.shared.dataTask(with: request) {(data, response, error) in
+                if let error = error {
+                    if (error as NSError).domain == NSURLErrorDomain,
+                        (error as NSError).code == NSURLErrorNotConnectedToInternet
+                    {
+                        completion(.failure(.offline))
+                    } else {
+                        completion(.failure(.system(error: error)))
+                    }
+                } else if (response as? HTTPURLResponse)?.statusCode != 200 {
+                    completion(.failure(.invalidStatusCode(code: (response as! HTTPURLResponse).statusCode, data: data)))
+                } else if let data = data {
+                    completion(.success(data))
+                } else {
+                    completion(.failure(.unknown))
+                }
+                }.resume()
+        } else {
+            completion(.failure(.invalidUrl(urlString: urlString)))
+        }
+    }
+    
+    class func responseObject<T: Decodable>(urlString: String,
+                                            completion: @escaping (Result<T, CustomError>) -> Void) {
+        
+        // T is any type which can be decodable to convert the response from backend.
+        responseData(urlString: urlString,
+                     completion: { result in
+                        switch result {
+                        case .success(let data):
+                            
+                            // decoding response data.
+                            let jsonDecoder = JSONDecoder()
+                            do {
+                                if let string = String(data: data, encoding: .ascii),
+                                    let data = string.data(using: .utf8) {
+                                    let response = try jsonDecoder.decode(T.self, from: data)
+                                    completion(.success(response))
+                                } else {
+                                    completion(.failure(.unknown))
+                                }
+                            } catch let error as DecodingError {
+                                completion(.failure(.decoding(data: data, error: error)))
+                            } catch {
+                                completion(.failure(.system(error: error)))
+                            }
+                        case .failure(let error):
+                            completion(.failure(error))
+                        }
+        })
+}
