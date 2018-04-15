ebb07187a14016af11f53ddfe0012f3b70e4cd84
Prashanthi Boravelly <prashanthi.boravelly@optus.com.au>
2018-04-15 15:15:44 +1000

    Created Fact.swift Added Fact struct.

---
+++ CollectionViewDemo/Model/Fact.swift
@@ -0,0 +1,25 @@
+//
+//  Fact.swift
+//  CollectionViewDemo
+//
+//  Created by Prashanthi Boravelly on 15/4/18.
+//  Copyright © 2018 Prashanthi Boravelly. All rights reserved.
+//
+
+import Foundation
+
+struct Fact: Decodable {
+    let title: String
+    let rows: [Row]?
+    struct Row: Decodable {
+        let title: String?
+        let description: String?
+        let imageURL: String?
+        
+        enum CodingKeys: String, CodingKey {
+            case title
+            case description
+            case imageURL = "imageHref"
+        }
+    }
+}
