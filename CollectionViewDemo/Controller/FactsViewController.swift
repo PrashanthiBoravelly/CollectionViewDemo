abff730a2bf76d933602ca6156e9d8da1f7cbc19
Prashanthi Boravelly <prashanthi.boravelly@optus.com.au>
2018-04-15 15:26:42 +1000

    Added refreshControl as a subview of collectionview. added IBAction for refreshcontrol.

--- CollectionViewDemo/Controller/FactsViewController.swift
+++ CollectionViewDemo/Controller/FactsViewController.swift
@@ -11,10 +11,21 @@ import UIKit
 private let reuseIdentifier = "Cell"
 
 class FactsViewController: UICollectionViewController {
+    
+    private lazy var refreshControl: UIRefreshControl = {
+        let refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action: #selector(refresh(_:)), for: .valueChanged)
+        return refreshControl
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        collectionView?.addSubview(refreshControl)
         showLoadingView()
+        refreshFacts()
+    }
+    
+    @IBAction func refresh(_ sender: UIRefreshControl) {
         refreshFacts()
     }
     
@@ -23,6 +34,7 @@ class FactsViewController: UICollectionViewController {
             DispatchQueue.main.async { [weak self] in
                 guard let strongSelf = self else { return }
                 strongSelf.hideLoadingView()
+                strongSelf.refreshControl.endRefreshing()
                 switch result {
                 case .success(let fact):
                     print(fact.title)
