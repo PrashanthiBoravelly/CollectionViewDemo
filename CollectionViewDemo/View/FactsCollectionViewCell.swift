9d4f4cac87d7ff921f9a7dc62c9c846fc394c153
Prashanthi Boravelly <prashanthi.boravelly@optus.com.au>
2018-04-15 15:21:10 +1000

    Adde Imageview, titleLabel IBOutlets to FactsCollectionViewCell.

--- CollectionViewDemo/View/FactsCollectionViewCell.swift
+++ CollectionViewDemo/View/FactsCollectionViewCell.swift
@@ -10,9 +10,8 @@ import UIKit
 
 class FactsCollectionViewCell: UICollectionViewCell {
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var activityIndicatorView: UIActivityIndicatorView!
 
 }
