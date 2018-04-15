c5e2625482fa35d046b0b5d9749dfff49ea25e89
Prashanthi Boravelly <prashanthi.boravelly@optus.com.au>
2018-04-15 15:19:37 +1000

    Created FactsCollectionViewCell.xib, FactsCollectionView.swift.

---
+++ CollectionViewDemo/View/FactsCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  FactsCollectionViewCell.swift
+//  CollectionViewDemo
+//
+//  Created by Prashanthi Boravelly on 15/4/18.
+//  Copyright © 2018 Prashanthi Boravelly. All rights reserved.
+//
+
+import UIKit
+
+class FactsCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
