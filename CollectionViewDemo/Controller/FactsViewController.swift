30e77f06e58b5f51982c94aa3597203ff75ca1ae
Prashanthi Boravelly <prashanthi.boravelly@optus.com.au>
2018-04-15 15:31:12 +1000

    Registering FactsCollectionViewCell with CollectionView. Added datasource methods for collectionview.

--- CollectionViewDemo/Controller/FactsViewController.swift
+++ CollectionViewDemo/Controller/FactsViewController.swift
@@ -22,6 +22,7 @@ class FactsViewController: UICollectionViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        collectionView?.register(UINib(nibName: "FactsCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "facts")
         collectionView?.addSubview(refreshControl)
         showLoadingView()
         refreshFacts()
@@ -48,6 +49,16 @@ class FactsViewController: UICollectionViewController {
         }
     }
    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return fact?.rows?.count ?? 0
+    }
     
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "facts", for: indexPath) as! FactsCollectionViewCell
+        let row = fact?.rows?[indexPath.row]
+        cell.titleLabel?.text = row?.title
+        cell.imageView.image = nil
+        return cell
+    }
    
 }
